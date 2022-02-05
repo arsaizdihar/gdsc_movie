f536df5f83a54adc035db5e74ddbadd87c013203
arsaizdihar <arsadihar@gmail.como>
2022-02-05 10:47:25 +0700

    Add wishlist page

--- README.md
+++ README.md
@@ -4,3 +4,7 @@
 - Arsa Izdihar Islam (16521134)
 
 Udah. Iya saya sendiri
+
+## Deploy link
+
+<a href="https://movie.arsaizdihar.com" target="_blank">Click here</a>
--- app/routes/wishlist.tsx
+++ app/routes/wishlist.tsx
@@ -1,3 +1,55 @@
+import { ActionFunction, LoaderFunction, redirect, useLoaderData } from "remix";
+import Movie, { IMovie } from "~/components/Movie";
+import { getUserId } from "~/utils/auth.server";
+import { db } from "~/utils/db.server";
+
+export const loader: LoaderFunction = async ({ request }) => {
+  const userId = await getUserId(request);
+  if (!userId) {
+    return redirect("/login?redirectTo=/wishlist");
+  }
+  const movies = await db.movie.findMany({
+    where: { wishlists: { some: { userId } } },
+    select: {
+      id: true,
+      title: true,
+      overview: true,
+      backdropPath: true,
+    },
+  });
+  return movies;
+};
+export const action: ActionFunction = async ({ request }) => {
+  const formData = await request.formData();
+  const movieId = Number(formData.get("movieId"));
+  if (isNaN(movieId)) {
+    return { error: true };
+  }
+  const userId = await getUserId(request);
+  if (!userId) return;
+
+  try {
+    await db.wishlist.delete({
+      where: { userId_movieId: { movieId, userId } },
+    });
+  } catch (error) {
+    return { error: true };
+  }
+  return { success: true };
+};
+
 export default function Wishlist() {
-  return <></>;
+  const movies = useLoaderData<IMovie[]>();
+  return (
+    <main className="pt-20 max-w-screen-xl mx-auto overflow-hidden px-4">
+      <h1 className="text-center text-3xl font-medium tracking-wide mt-12">
+        Your Movie Wishlist
+      </h1>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mt-12">
+        {movies.map((movie) => (
+          <Movie key={movie.id} {...movie} isWishlist />
+        ))}
+      </div>
+    </main>
+  );
 }
